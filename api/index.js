7bdc2a09e5e9c04383b2fadd9a03c024b510dc76
Mohammed Alluhaydan <86924750+MohammedTech-01@users.noreply.github.com>
2024-05-01 17:14:12 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const TOKEN_SECRET_KEY = process.env.TOKEN_SECRET_KEY;
 app.use(
   cors({
     credentials: true,
-    origin: `https://blog-app-api2.vercel.app/`,
+    origin: `https://fcit-blog.vercel.app/`,
   })
 );
 app.use(express.json());
